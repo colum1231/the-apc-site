7ef56f36920e6a0dd99b5867855fa40d60db0fa6
colum1231 <colum@theaplayersclub.com>
2025-09-03 11:39:20 +0000

    add /hq and /search pages

--- web/app/hq/page.tsx
+++ web/app/hq/page.tsx
@@ -1,7 +1,3 @@
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-black text-white p-8">
-      HQ Page
-    </main>
-  );
+export default function HQ(){
+  return <main className="min-h-screen bg-black text-white p-8">HQ</main>;
 }
